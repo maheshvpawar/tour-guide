2e56db3c85d8b67e10f3a5824ad881ee29cdc170
Mahesh V Pawar <pawarmahesh723@gmail.com>
2020-12-10 21:32:25 +0530

    Implemented Activities and Fragements

---
+++ app/src/main/java/com/example/maheshvpawar/tourguide/ParksActivity.java
@@ -0,0 +1,15 @@
+    package com.example.maheshvpawar.tourguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ParksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+
+    }
+}
---
+++ app/src/main/java/com/example/maheshvpawar/tourguide/ShoppingFragment.java
@@ -0,0 +1,17 @@
+package com.example.maheshvpawar.tourguide;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ShoppingFragment extends Fragment {
+
+
+}
